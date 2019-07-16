030c59f2f98fbee1cb91fbcdee4928bb6e9e5c5b
britech angyerts <brian.angyang@gmail.com>
2019-07-16 22:13:01 +0800

    add utility class

---
+++ cpanel/src/utils/ApplicationUtils.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace gov\pglu\tourism\util;
+
+final class ApplicationUtils {
+
+    const TOURISM_CIRCUITS = [
+        'Northern' => ['Balaoan', 'Bangar', 'Luna', 'Santol', 'Sudipen'],
+        'Central' => ['Bacnotan', 'San Fernando', 'San Gabriel', 'San Juan'],
+        'Central Eastern' => ['Bauang', 'Bagulin', 'Burgos', 'Naguilian'],
+        'South' => ['Agoo', 'Aringay', 'Caba', 'Santo Tomas'],
+        'South Eastern' => ['Pugo', 'Rosario', 'Tubao']
+    ];
+}
