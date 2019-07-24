56cdf23eca6e9e8dffb58115876a3febfc9430a7
Brian Angyang <brian.angyang@gmail.com>
2019-07-24 16:51:31 +0800

    rename function to reflect with interface and add implementation for visitor count

--- src/services/OpenDataServiceCsvImpl.php
+++ src/services/OpenDataServiceCsvImpl.php
@@ -3,17 +3,21 @@
 namespace gov\pglu\tourism\service;
 
 use gov\pglu\tourism\service\OpenDataService;
-use gov\pglu\tourism\dao\PoiManagementDaoImpl as PoiManagementDao;
+use gov\pglu\tourism\dao\PoiManagementDao;
+use gov\pglu\tourism\dao\OpenDataDao;
 
 /**
  * @property PoiManagementDao $poiManagementDao
+ * @property OpenDataDao $openDataDao
  */
 class OpenDataServiceCsvImpl implements OpenDataService {
 
     private $poiManagementDao;
+    private $openDataDao;
 
-    public function __construct(PoiManagementDao $poiManagementDao) {
+    public function __construct(PoiManagementDao $poiManagementDao, OpenDataDao $openDataDao) {
         $this->poiManagementDao = $poiManagementDao;
+        $this->openDataDao = $openDataDao;
     }
 
     public function listPoi() {
@@ -37,10 +41,21 @@ class OpenDataServiceCsvImpl implements OpenDataService {
     }
 
     public function countVisitors(array $criteriaMap) {
+        $csvRows = [['Name', 'Visitor Count']];
+        try {
+            $rows = $this->openDataDao->countVisitors($criteriaMap);
+            foreach($rows as $row) {
+                list('name' => $name, 'visitorcount' => $visitorCount) = $row;
+                $csvRows = array_merge($csvRows, [[$name, $visitorCount]]);
+            }
+            return $csvRows;
+        } catch (\PDOException $ex) {
+            throw $ex;
+        }
         return [];
     }
 
-    public function computeVisitorAverageRating(array $criteriaMap) {
+    public function computeAverageVisitorRating(array $criteriaMap) {
         return [];
     }
 }
