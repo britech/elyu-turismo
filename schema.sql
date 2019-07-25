53ab0ddc6fb0755e3ebbaa7a087190a4320a19e6
britech angyerts <brian.angyang@gmail.com>
2019-07-25 20:46:02 +0800

    update schema

--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
 --
--- Host: localhost    Database: tourism
+-- Host: 192.168.99.100    Database: tourism
 -- ------------------------------------------------------
--- Server version	5.6.42
+-- Server version	5.7.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,23 +31,6 @@ CREATE TABLE `classification` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `guest`
---
-
-DROP TABLE IF EXISTS `guest`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `guest` (
-  `id` varchar(50) NOT NULL,
-  `ipaddress` varchar(50) DEFAULT NULL,
-  `latitude` varchar(100) DEFAULT NULL,
-  `longitude` varchar(100) DEFAULT NULL,
-  `daterecorded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `placeofinterest`
 --
 
@@ -67,9 +50,9 @@ CREATE TABLE `placeofinterest` (
   `displayable` tinyint(4) DEFAULT '0',
   `descriptionWysiwyg` json DEFAULT NULL,
   `commuterGuideWysiwyg` json DEFAULT NULL,
-  `overview` varchar(200) DEFAULT NULL,
+  `imageName` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -86,29 +69,6 @@ CREATE TABLE `poiclassification` (
   KEY `poiclassification_fk_1` (`classification`),
   CONSTRAINT `poiclassification_fk` FOREIGN KEY (`placeofinterest`) REFERENCES `placeofinterest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `poiclassification_fk_1` FOREIGN KEY (`classification`) REFERENCES `classification` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `poidiscovery`
---
-
-DROP TABLE IF EXISTS `poidiscovery`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `poidiscovery` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(1000) NOT NULL,
-  `latitude` varchar(100) NOT NULL,
-  `longitude` varchar(100) NOT NULL,
-  `guest` varchar(50) DEFAULT NULL,
-  `rating` tinyint(4) DEFAULT NULL,
-  `comments` text,
-  `status` varchar(50) DEFAULT 'for_review',
-  `dateadded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `poidiscovery_fk` (`guest`),
-  CONSTRAINT `poidiscovery_fk` FOREIGN KEY (`guest`) REFERENCES `guest` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -132,28 +92,6 @@ CREATE TABLE `poifee` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `poirating`
---
-
-DROP TABLE IF EXISTS `poirating`;
-CREATE TABLE `poirating` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `placeofinterest` int(11) NOT NULL,
-  `rating` tinyint(4) NOT NULL,
-  `guest` varchar(50) DEFAULT NULL,
-  `comments` text,
-  `status` varchar(50) DEFAULT 'for_review',
-  `dateadded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `poirating_fk` (`placeofinterest`),
-  KEY `poirating_fk_1` (`guest`),
-  KEY `poirating_alternate_search_IDX` (`placeofinterest`,`dateadded`) USING BTREE,
-  KEY `poirating_primary_search_IDX` (`dateadded`) USING BTREE,
-  CONSTRAINT `poirating_fk` FOREIGN KEY (`placeofinterest`) REFERENCES `placeofinterest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `poirating_fk_1` FOREIGN KEY (`guest`) REFERENCES `guest` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
 -- Table structure for table `poischedule`
 --
 
@@ -163,8 +101,8 @@ DROP TABLE IF EXISTS `poischedule`;
 CREATE TABLE `poischedule` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `placeofinterest` int(11) NOT NULL,
-  `startingtime` time NOT NULL,
-  `closingtime` time NOT NULL,
+  `startingtime` time DEFAULT NULL,
+  `closingtime` time DEFAULT NULL,
   `date` date DEFAULT NULL,
   `day` varchar(100) DEFAULT NULL,
   `notes` varchar(1000) DEFAULT NULL,
@@ -199,17 +137,17 @@ CREATE TABLE `poitag` (
 --
 
 DROP TABLE IF EXISTS `poivisit`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `poivisit` (
   `placeofinterest` int(11) NOT NULL,
-  `guest` varchar(50) DEFAULT NULL,
   `dateadded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   KEY `poivisit_fk` (`placeofinterest`),
-  KEY `poivisit_fk_1` (`guest`),
   KEY `poivisit_alternate_search_IDX` (`placeofinterest`,`dateadded`) USING BTREE,
   KEY `poivisit_primary_search_IDX` (`dateadded`) USING BTREE,
-  CONSTRAINT `poivisit_fk` FOREIGN KEY (`placeofinterest`) REFERENCES `placeofinterest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `poivisit_fk_1` FOREIGN KEY (`guest`) REFERENCES `guest` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+  CONSTRAINT `poivisit_fk` FOREIGN KEY (`placeofinterest`) REFERENCES `placeofinterest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `topictag`
@@ -226,23 +164,6 @@ CREATE TABLE `topictag` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `poiimage` (
-  `id` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `placeofinterest` int(11) NOT NULL,
-  `designation` varchar(50) CHARACTER SET latin1 NOT NULL,
-  `enabled` tinyint(4) DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `poiimage_FK` (`placeofinterest`),
-  CONSTRAINT `poiimage_FK` FOREIGN KEY (`placeofinterest`) REFERENCES `placeofinterest` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE USER 'tourism_user'@'localhost' IDENTIFIED BY 'u&e6uaAz-b#^Lj7m';
-GRANT SELECT,INSERT,UPDATE,DELETE ON tourism.* TO 'tourism_user'@'localhost';
-
-CREATE USER 'tourism_user'@'%' IDENTIFIED BY 'u&e6uaAz-b#^Lj7m';
-GRANT SELECT,INSERT,UPDATE,DELETE ON tourism.* TO 'tourism_user'@'%';
-
-FLUSH PRIVILEGES;
 --
 -- Dumping routines for database 'tourism'
 --
@@ -256,4 +177,11 @@ FLUSH PRIVILEGES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-15  6:20:44
+-- Dump completed on 2019-07-25 20:34:17
+CREATE USER 'tourism_user'@'localhost' IDENTIFIED BY 'u&e6uaAz-b#^Lj7m';
+GRANT SELECT,INSERT,UPDATE,DELETE ON tourism.* TO 'tourism_user'@'localhost';
+
+CREATE USER 'tourism_user'@'%' IDENTIFIED BY 'u&e6uaAz-b#^Lj7m';
+GRANT SELECT,INSERT,UPDATE,DELETE ON tourism.* TO 'tourism_user'@'%';
+
+FLUSH PRIVILEGES;
