23c2bea29bc8102a8406bf21b798c734a1f00e96
britech angyerts <brian.angyang@gmail.com>
2019-07-28 22:14:52 +0800

    remove query for computation of rating

--- src/data/OpenDataDao.php
+++ src/data/OpenDataDao.php
@@ -5,6 +5,4 @@ namespace gov\pglu\tourism\dao;
 interface OpenDataDao {
 
     public function countVisitors(array $criteria);
-
-    public function computeAverageVisitorRating(array $criteria);
 }
