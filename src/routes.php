d6d56209a213a3a09480a305846795d1e2ae9d10
Brian Angyang <brian.angyang@outlook.com>
2019-08-05 12:58:50 +0800

    additional logging

--- src/routes.php
+++ src/routes.php
@@ -784,7 +784,9 @@ return function (App $app) {
             $summaryResult = $container->openDataDao->summarizeVisitors();
             foreach($towns as $town) {
                 $result = array_filter($summaryResult, function($val) use ($town) {
+                    $this->logger->debug("Current Town => {$town}");
                     list('town' => $resultTown) = $val;
+                    $this->logger->debug("Town => {$resultTown}");
                     return strcasecmp($town, $resultTown) == 0;
                 });
                 $this->logger->debug(json_encode($summaryResult));
